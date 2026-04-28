5cfdabc108e6466883afd42ac6e3b384449ab213
BroerSakki <besterizak@gmail.com>
2026-04-28 14:33:27 +0200

    Typing fix and threading capabilities

--- src/orcstrate/core/command.py
+++ src/orcstrate/core/command.py
@@ -1,6 +1,6 @@
 class Command:
-    def __init__(self, command, external=False, keep_open=True, name=None):
-        self.command = command
-        self.external = external
-        self.keep_open = keep_open
-        self.name = name or command
+    def __init__(self, command:str, external:bool=False, keep_open:bool=True, name:str=None):
+        self.command:str = command
+        self.external:bool = external
+        self.keep_open:bool = keep_open
+        self.name:str = name or command
