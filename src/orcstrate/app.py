b9210293795af8fae9c9f70872a0a4384ed06ab6
Morne Ras <rascustom123@gmail.com>
2026-05-01 01:09:26 +0200

    ui: Added an example/tutorial widget to guide future widgets

--- src/orcstrate/app.py
+++ src/orcstrate/app.py
@@ -2,17 +2,19 @@ import gi
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk
 from ui.window import MainWindow
+from models.command import Command
 
 class App(Gtk.Application):
     # Constructor
     # ---
-    def __init__(self):
+    def __init__(self, commands: list[Command]):
         super().__init__(application_id="com.orcstrate.app")
+        self.commands = commands
     # ---
 
     # App runner
     # ---
     def do_activate(self):
-        win = MainWindow(self)
+        win = MainWindow(self, self.commands)
         win.present()
     # ---
--- src/orcstrate/ui/window.py
+++ src/orcstrate/ui/window.py
@@ -1,16 +1,23 @@
 import gi
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk
+from ui.list_search_widget import ListSearchWidget
+from models.command import Command
 
 class MainWindow(Gtk.ApplicationWindow):
     # Constructor
     # ---
-    def __init__(self, app):
+    def __init__(self, app, commands: list[Command]):
         super().__init__(application=app, title="Orcstrate")
 
         # Root layout
         # ---
         self.root = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
         self.set_child(self.root)
+        # ---
+        
+        # List Search Widget
+        # ---
+        ListSearchWidget(self.root, commands)
         # ---
     # ---
