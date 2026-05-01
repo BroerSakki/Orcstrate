b70ee4ed31838f9e478edf20b077a23c2fd45a9a
Morne Ras <rascustom123@gmail.com>
2026-05-02 00:23:51 +0200

    widget: Dark mode didn't work correctly, should work now

--- src/orcstrate/app.py
+++ src/orcstrate/app.py
@@ -16,5 +16,23 @@ class App(Gtk.Application):
     # ---
     def do_activate(self):
         win = MainWindow(self, self.commands)
+        
+        def update_theme_class(*args):
+            # Check if the system prefers dark
+            settings = Gtk.Settings.get_default()
+            is_dark = settings.get_property("gtk-application-prefer-dark-theme")
+            
+            # Also check for Libadwaita style system preference
+            # (Needed for modern GNOME)
+            if is_dark:
+                win.add_css_class("dark-mode")
+            else:
+                win.remove_css_class("dark-mode")
+                
+        settings = Gtk.Settings.get_default()
+        settings.connect("notify::gtk-application-prefer-dark-theme", update_theme_class)
+        
+        update_theme_class()
+
         win.present()
     # ---
