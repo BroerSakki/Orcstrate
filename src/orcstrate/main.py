3fb6de02371f81701a4ead4c76ba5271b873f12c
BroerSakki <besterizak@gmail.com>
2026-05-10 21:56:46 +0200

    Tweak core features

--- src/orcstrate/main.py
+++ src/orcstrate/main.py
@@ -3,31 +3,5 @@ from models.command import Command
 from app import App
 import time
 
-runner = CommandRunner()
-commands = [
-    Command("echo '=== START ==='"),
-    Command("echo 'Running internal command 1'"),
-    Command("sleep 5", external=True),
-    Command("echo 'Back to internal flow'"),
-    Command("ping -c 4 google.com", external=True, keep_open=False),
-    Command("sleep 2"),
-    Command("ls /this/does/not/exist"),
-    Command("echo '=== END ==='")
-]
-
-runner.load_commands(commands)
-
-print("[INFO] Press Enter to stop...\n")
-
-runner.add_to_queue(Command("echo thingymajiggle"))
-#runner.run_queue()
-
-
-time.sleep(1)
-
-runner.add_to_queue(Command("echo added later"))
-
-#runner.wait_until_done(True)
-
-app = App(runner.queue)
+app = App()
 app.run()
