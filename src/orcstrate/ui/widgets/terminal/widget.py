16e2f93c4fbcd0a57d3df8548f69f777774e6cd4
Morne Ras <rascustom123@gmail.com>
2026-05-13 21:02:48 +0000

    terminal: Rough start to built in terminal in ui

--- src/orcstrate/ui/widgets/terminal/widget.py
+++ src/orcstrate/ui/widgets/terminal/widget.py
@@ -6,23 +6,66 @@ from gi.repository import GLib
 from gi.repository import Vte
 import os
 
+from core.queue_service import (
+	QueueService
+)
+
 class TerminalWidget(Gtk.Box):
-	def __init__(self):
+	def __init__(self, queue_service):
+		super().__init__(
+			orientation=Gtk.Orientation.VERTICAL,
+			spacing=12
+		)
+
+		self.queue_service:QueueService = queue_service
+
+		self.terminal = Vte.Terminal()
+
+		shell_bin = os.environ.get("SHELL", "/bin/sh")
+		argv = [shell_bin]
+		working_dir = os.environ.get("HOME", "/")
+
+		self.terminal.spawn_async(
+			Vte.PtyFlags.DEFAULT,
+			working_dir,
+			argv,
+			None,
+			GLib.SpawnFlags.DEFAULT,
+			None, None,
+			-1,
+			None,
+			None,
+			None
+		)
+
+		self.build_ui()
+
+	def build_ui(self):
 		box = Gtk.Box(
 			orientation=Gtk.Orientation.HORIZONTAL,
 			spacing=8
 		)
 
-		terminal = Vte.Terminal()
-		terminal.spawn_async(
-			Vte.PtyFlags.DEFAULT,
-			os.environ['HOME'],
-			["/bin/bash"],
-			[],
-			GLib.SpawnFlags.DO_NOT_REAP_CHILD,
-			None, None, -1, None, None
+		box.append(self.terminal)
+
+		play_btn = Gtk.Button(
+			label="Play Queue",
+			icon_name="media-playback-start-symbolic"
 		)
 
-		box.append(terminal)
+		play_btn.add_css_class(
+			"suggested-action"
+		)
+
+		play_btn.connect(
+			"clicked",
+			self.on_play_clicked
+		)
+
+		box.append(play_btn)
 
 		self.append(box)
+
+	def on_play_clicked(self, btn):
+		self.queue_service.run_queue(self.terminal)
+		#self.run_internal("echo Test")
